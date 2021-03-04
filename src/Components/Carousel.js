d4fc4df8f8461dacbbaf7beab6de2321ee74ba95
Arjit Bhandari <arjitbhandari222830@gmail.com>
2021-03-05 00:37:38 +0530

    Adding carousel

--- src/Components/Carousel.js
+++ src/Components/Carousel.js
@@ -1,15 +1,36 @@
-import React,{useContext} from 'react';
+import React,{useState,useContext, useEffect} from 'react';
+import {carouselData} from '../helpers/carouselData';
+import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
+import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
 
 import {AppContext} from "../AppContext";
 
+const data = carouselData();
 
 const Carousel= ()=> {
 
     const {state}=  useContext(AppContext);
+  
+    const [show,setShow]= useState(0);
+
+    useEffect(()=>{
+        
+        const interval = setInterval(() => {
+            setShow(prev=>prev==data.length-1?0:prev+1)
+        },5000)
+      
+        return () => {
+        clearInterval(interval);
+        };
+    },[]);
+
+    console.log(show);
 
     return (
         <div>
-            Carousel
+            <ArrowBackIosIcon style={{position: 'absolute',marginTop:state.mobileView?'12vh':'20vh'}} onClick={()=>setShow(prev=>show==0?data.length-1:prev-1)} />
+            <img src={data[show].link} style={{width:'100vw',height: state.mobileView?'30vh':'50vh'}} />
+            <ArrowForwardIosIcon style={{position: 'absolute',marginLeft:state.mobileView?'-6vw':'-2vw',marginTop:state.mobileView?'12vh':'20vh'}} onClick={()=>setShow(prev=>show==data.length-1?0:prev+1)}/>
         </div>
     );
 }
