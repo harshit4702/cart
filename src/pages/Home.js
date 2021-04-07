60992da15f0c28b9021c1af89a2413664daf235e
Arjit Bhandari <arjitbhandari222830@gmail.com>
2021-04-07 16:30:10 +0530

    Adding Carts

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,37 +1,53 @@
 import React,{useContext} from 'react';
 import {Link} from 'react-router-dom';
 import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
 
 import Carousel from '../Components/Carousel';
 import Categories from '../Components/Categories';
 import RowImages from '../Components/RowImages';
+import MediaCard from '../Components/MediaCard';
+
 import {AppContext} from '../AppContext';
 
+const useStyles = makeStyles({
+  handle: {
+    backgroundColor: 'white',
+    display: "flex",
+    flexDirection: "row",
+    flexWrap: "nowrap",
+    margin: 1,
+    padding: 1,
+    width:'100%',
+    float: 'left',
+    maxHeight: 300,
+    overflowX: 'scroll'
+  }
+});
+
 const Home= ()=> {
+
+  const classes = useStyles();
 
   const {state,dispatch}= useContext(AppContext);
 
   console.log(state);
-  var x=0;
+
   return (
     <div>
         <Carousel />
         <br/>
-        <Grid container spacing={3} justify="center" alignContent="space-between" alignItems="center">
-        { 
-          state.products && Object.values(state.products).map((item,index)=>{
-            return (
-              <Grid item key={index}>
-                <Link to={`/product/${item._id}`} style={{textDecoration:'none',color: 'black'}}>
-                  <p style={{cursor:'pointer'}}>
-                      Category {++x}
-                  </p> 
+        <div className={classes.handle} style={{overflowX:'scroll'}} >
+          { 
+            state.products && Object.values(state.products).map((item,index)=>{
+              return (
+                <Link to={`/product/${item._id}`}>
+                  <MediaCard product={item}/>
                 </Link>
-              </Grid>
-            );
-          })
-        }
-        </Grid>
+              );
+            })
+          }
+        </div>
         <br/>
         <RowImages />
     </div>
